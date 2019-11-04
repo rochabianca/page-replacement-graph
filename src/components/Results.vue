b826280c6ccfc7fa12862925bb89729358bf1923
rochabianca <bianca.rocha1711@gmail.com>
2019-11-04 09:52:44 -0300

    [WIP] starting fifo algoritnm

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -7,22 +7,59 @@
 <script>
 export default {
   name: 'Results',
+  created() {
+    this.FIFO(5);
+  },
   data() {
     return {
       algorithm: '7W-2W-7R-4W-4R-2R-6R-6R-5W-2W-7R-0R-5W-6W-4R-5R-1R-1W-5W-',
+      faltas: 0,
+      acertos: 0,
+      memory: [],
       frames: [5, 10]
     }
   },
   methods: {
-    FIFO(alg) {
-      console.log(alg)
+    FIFO(frames) {
+      let acertos = 0;
+      let faltas = 0;
+      let memory = [];
+      console.log('fifo')
+      for (let i = 0; i < this.algorithmArray.length; i++) {
+        if (memory.includes(this.algorithmArray[i].value)) {
+          acertos++;
+        } else {
+          faltas++;
+          if (memory.length < frames) {
+            memory.push(this.algorithmArray[i].value);
+          } else {
+            memory.splice(0, 1)
+            memory.push(this.algorithmArray[i].value);
+          }
+        }
+      }
+      console.log('acertos: ', acertos);
+      console.log('faltas: ', faltas);
+      console.log('memoria final: ', memory);
+      return {
+        acertos,
+        memory,
+        algoritmo: 'FIFO'
+      }
     }
   },
   computed: {
     algorithmArray() {
       let response = this.algorithm.split('-');
+      let responseArray = [];
       response.pop()
-      return response;
+      for (let i = 0; i < response.length; i++) {
+        responseArray.push({
+          value: response[i].slice(0, (response[i].length - 1)),
+          type: response[i].slice((response[i].length - 1), response[i].length),
+        })
+      }
+      return responseArray;
     }
   }
 
